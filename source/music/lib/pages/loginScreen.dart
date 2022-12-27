6cf3c44e70b6c061c45adafdb9b52bbef0f2e114
Aviral yadav <58760825+yaviral17@users.noreply.github.com>
2022-12-27 15:32:26 +0530

    fixed over flow issue

--- source/music/lib/pages/loginScreen.dart
+++ source/music/lib/pages/loginScreen.dart
@@ -21,41 +21,72 @@ class _loginScreen_State extends State<loginScreen_> {
   Widget build(BuildContext context) {
     var display = MediaQuery.of(context).size;
     return Material(
-      child: SingleChildScrollView(
-        child: Container(
-          height: display.height,
-          width: display.width,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  decoration:
-                      BoxDecoration(border: Border.all(color: Colors.white)),
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(top: 10.0),
-                  width: display.width > 500 ? 500 : display.width,
-                  child: Lottie.asset(boyListning, fit: BoxFit.fitWidth),
+      child: Container(
+        height: display.height,
+        width: display.width,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.white)),
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(top: 10.0),
+                width: display.width > 500 ? 500 : display.width,
+                child: Lottie.asset(boyListning, fit: BoxFit.fitWidth),
+              ),
+              SizedBox(
+                height: display.height * (6 / 100),
+              ),
+              const Text(
+                "Login",
+                style: TextStyle(
+                  fontFamily: "Josefin Sans",
+                  fontWeight: FontWeight.w800,
+                  fontSize: 42.0,
                 ),
-                SizedBox(
-                  height: display.height * (6 / 100),
-                ),
-                const Text(
-                  "Login",
-                  style: TextStyle(
-                    fontFamily: "Josefin Sans",
-                    fontWeight: FontWeight.w800,
-                    fontSize: 42.0,
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Container(
-                  child: Column(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      Container(
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                child: Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Container(
+                      width: display.width > 500
+                          ? 350
+                          : display.width * (85 / 100),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(4),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            // spreadRadius: 1,
+                            blurRadius: 2,
+                          )
+                        ],
+                      ),
+                      child: TextFormField(
+                        controller: username,
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.only(top: 16),
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          hintText: "Email",
+                          prefixIcon: Icon(
+                            Icons.alternate_email,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Center(
+                      child: Container(
                         width: display.width > 500
                             ? 350
                             : display.width * (85 / 100),
@@ -70,138 +101,115 @@ class _loginScreen_State extends State<loginScreen_> {
                             )
                           ],
                         ),
-                        child: TextFormField(
-                          controller: username,
-                          decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.only(top: 16),
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            hintText: "Email",
-                            prefixIcon: Icon(
-                              Icons.alternate_email,
+                        child: Stack(
+                          children: [
+                            TextFormField(
+                              controller: password,
+                              decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.only(top: 16),
+                                focusedBorder: InputBorder.none,
+                                enabledBorder: InputBorder.none,
+                                hintText: "Password",
+                                prefixIcon: Icon(
+                                  Icons.lock_outline_rounded,
+                                ),
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      Center(
-                        child: Container(
-                          width: display.width > 500
-                              ? 350
-                              : display.width * (85 / 100),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(4),
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.grey,
-                                // spreadRadius: 1,
-                                blurRadius: 2,
-                              )
-                            ],
-                          ),
-                          child: Stack(
-                            children: [
-                              TextFormField(
-                                controller: password,
-                                decoration: const InputDecoration(
-                                  contentPadding: EdgeInsets.only(top: 16),
-                                  focusedBorder: InputBorder.none,
-                                  enabledBorder: InputBorder.none,
-                                  hintText: "Password",
-                                  prefixIcon: Icon(
-                                    Icons.lock_outline_rounded,
-                                  ),
-                                ),
+                            Container(
+                              height: 44,
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: const Text("Forget?"),
                               ),
-                              Container(
-                                height: 44,
-                                alignment: Alignment.centerRight,
-                                child: TextButton(
-                                  onPressed: () {},
-                                  child: const Text("Forget?"),
-                                ),
-                              ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height:16,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage(),));
-                        },
-                        child: roundedEdgeButton(
-                          buttonHeight: 48,
-                          buttonWidth: display.width > 500? 300 :display.width * (60 / 100),
-                          lableText: "Login",
-                          lableTextStyle: regulatInnerButtonStyle,
-                          buttonEdgeRadius: 12,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 16,
-                      ),
-                      const Text(
-                        "Or, login with...",
-                        style: regularPageNote,
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    authIcons(
-                      innerImage: 'assets/images/google_logo.png',
                     ),
                     SizedBox(
-                      width: 12,
+                      height: 16,
                     ),
-                    authIcons(
-                      innerImage: 'assets/images/facebook_logo.png',
-                    ),
-                    SizedBox(
-                      width: 12,
-                    ),
-                    authIcons(
-                      innerImage: 'assets/images/apple_logo.png',
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: display.height * (5 / 100),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text("Don't have an account ?"),
                     GestureDetector(
                       onTap: () {
-                         Navigator.push(context, MaterialPageRoute(builder: (context) => const RegisterScreen(),));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomePage(),
+                            ));
                       },
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const RegisterScreen(),
-                              ));
-                        },
-                        child: Text("Register"),
+                      child: roundedEdgeButton(
+                        buttonHeight: 48,
+                        buttonWidth: display.width > 500
+                            ? 300
+                            : display.width * (60 / 100),
+                        lableText: "Login",
+                        lableTextStyle: regulatInnerButtonStyle,
+                        buttonEdgeRadius: 12,
                       ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      "Or, login with...",
+                      style: regularPageNote,
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  authIcons(
+                    innerImage: 'assets/images/google_logo.png',
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  authIcons(
+                    innerImage: 'assets/images/facebook_logo.png',
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  authIcons(
+                    innerImage: 'assets/images/apple_logo.png',
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: display.height * (5 / 100),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account ?"),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ));
+                    },
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const RegisterScreen(),
+                            ));
+                      },
+                      child: Text("Register"),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
